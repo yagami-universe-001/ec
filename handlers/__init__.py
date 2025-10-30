d58b99d50f8ff22730d5a0d228555fea183ab7d5
yagami-universe-001 <yagamileo82@gmail.com>
2025-10-30 21:43:16 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,14 @@
+# handlers/__init__.py
+from .start import *
+from .encoding import *
+from .thumbnail import *
+from .watermark import *
+from .subtitle import *
+from .audio import *
+from .merge import *
+from .extract import *
+from .rename import *
+from .unzip import *
+from .mediainfo import *
+from .admin import *
+from .callbacks import *
